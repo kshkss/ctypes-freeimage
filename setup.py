f7796f04805d98956f7f57eea143bc852ae1abae
Akishika Issui <kshkss@users.noreply.github.com>
2020-01-11 13:03:43 +0900

    Refactoring setup.py and setup.cfg
    Remove options from setup.cfg exclude metadata.
    Setup.py includes package descriptions.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     python_requires='>=3.6, <4',
     install_requires=['numpy>=1.17'],
 )
